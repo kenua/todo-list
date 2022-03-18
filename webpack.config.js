5f80bf0fccea927e130f9122d263edb9aad6c16f
Kenua <jgvezon@gmail.com>
2022-03-18 07:52:00 -0500

    Add sidebar design
    Added loaders, plugins, and a configuration file for webpack to handle
    all the project files and installed the sass compiler and loader to
    create the style of the sidebar.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   mode: 'development',
+   devtool: 'inline-source-map',
+   devServer: {
+      static: './dist',
+      open: false,
+   },
+   entry: './src/index.js',
+   output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+   },
+   module: {
+      rules: [
+         {
+            test: /\.s[ac]ss$/i,
+            use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+            ],
+         },
+      ],
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         title: 'Todo List',
+         template: './src/template/index.ejs',
+      })
+   ],
+};
