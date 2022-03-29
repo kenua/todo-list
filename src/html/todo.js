5798e8570a26786582992adac23ce85e4a0b51b8
Kenua <jgvezon@gmail.com>
2022-03-29 11:46:42 -0500

    Remove commented html markup

--- src/html/todo.js
+++ src/html/todo.js
@@ -27,50 +27,8 @@ export default function appendTodo() {
          <span id="todo-title"></span>
       </h1>
    </div>
-
-<!--
-   <div class="todo__top">
-      <h1 class="todo__title text-center mb-4 mb-lg-0">
-         <span id="todo-title" class="fs-1 fw-bold text-capitalize"></span>
-         <span id="todo-reminder" class="fs-4 fw-light text-uppercase"></span>
-      </h1>
-
-      <ul class="list-unstyled todo__options  d-flex align-items-center m-0">
-         <li class="me-2">
-            <button id="mark-todo" class="options__button" title="Mark todo">
-               <!--<i class="fa-solid fa-circle"></i></i>->
-               <i id="mark-symbol" class="fa-regular fa-circle"></i>
-            </button>
-         </li>
-         <li class="me-2">
-            <button id="edit-todo" class="options__button" title="Edit todo">
-               <i class="fa-solid fa-pen-to-square"></i>
-            </button>
-         </li>
-         <li>
-            <button id="delete-todo" class="options__button" title="Delete todo">
-               <i class="fa-solid fa-trash-can"></i></i>
-            </button>
-         </li>
-      </ul>
-   </div>
-   -->
-   <div id="todo-content">
-   <!--
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni aliquid enim officia culpa veniam laboriosam. Ab, fugiat eaque aut sapiente labore repudiandae iste veniam tempora quod ipsa qui delectus perspiciatis dicta tenetur amet quasi illum, doloremque dolorum culpa. Similique, minima numquam. Esse voluptate reiciendis omnis deserunt! Consectetur, reiciendis debitis?</p>
-   -->
-   </div>
+   
+   <div id="todo-content"></div>
    `;
    document.querySelector('#content').append(todo);
 }
