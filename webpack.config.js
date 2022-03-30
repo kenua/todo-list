6c4c6a28c652aa71f8f17221c3fb75e6f09d36d8
Kenua <jgvezon@gmail.com>
2022-03-30 07:48:49 -0500

    Rename dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
    mode: 'development',
    devtool: 'inline-source-map',
    devServer: {
-      static: './dist',
+      static: './docs',
       open: false,
    },
    entry: './src/index.js',
    output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'docs'),
       clean: true,
    },
    module: {
